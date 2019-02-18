a22b67d21257d2fc35f5c5ca9e88ba02ab2254e4
Dinne Bosman <dwjbosman@yahoo.com>
2019-02-18 21:26:02 +0000

    updated dts files

--- yocto/build.sh
+++ yocto/build.sh
@@ -3,7 +3,7 @@
 set -ex
 
 CUR_DIR=$(pwd)
-OUTPUT_DIR=../build_env2
+OUTPUT_DIR=../yocto_build_env
 mkdir $OUTPUT_DIR
 cd $OUTPUT_DIR
 OUTPUT_DIR=$(pwd)
@@ -22,7 +22,7 @@ source oe-init-build-env
 bitbake-layers add-layer "$OUTPUT_DIR/poky/meta-xilinx/meta-xilinx-bsp"
 echo "MACHINE ??= \"zedboard-zynq7\"" >> $OUTPUT_DIR/poky/build/conf/local.conf
 bitbake core-image-minimal
-bitbake-layers add-layer "$OUTPUT_DIR/meta-dts"
+bitbake-layers add-layer "$OUTPUT_DIR/poky/meta-dts"
 #need to remove repodata dir, otherwise we'll get a mv error (the target dir is not empty...)
 rm -rf $OUTPUT_DIR/poky/build/tmp/work/zedboard_zynq7-poky-linux-gnueabi/core-image-minimal/1.0-r0/oe-rootfs-repo/repodata
 bitbake core-image-minimal 
